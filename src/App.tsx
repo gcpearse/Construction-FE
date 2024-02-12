9607544d243c787c3035f195f53c108b517db00c
gcpearse <glen.c.pearse@gmail.com>
2024-02-12 21:19:04 +0000

    builds inital routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom"
 import "./App.css"
+import Home from "./pages/Home"
 
 const App: React.FC = () => {
   return (
-    <h1>Construction</h1>
+    <main>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+      </Routes>
+    </main>
   )
 }
 
