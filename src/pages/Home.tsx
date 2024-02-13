015b025315994fd5aa8ed15f3e03e724e15535a3
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 00:56:58 +0000

    empty placeholder div added to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,6 @@
-import { useAppSelector } from "../app/hooks"
-
 const Home: React.FC = () => {
-
-  const isSidebarVisible = useAppSelector((state) => state.sidebar.visibleSidebar)
-
   return (
-    <div className={isSidebarVisible ? (
-      "sidebar visible-sidebar"
-    ) : (
-      "sidebar"
-    )}>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Jobs</li>
-        <li>Admin</li>
-      </ul>
-    </div>
+    <div></div>
   )
 }
 
