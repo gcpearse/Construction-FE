0149fe31d0be10df13d27f6bd53f4e2801e95444
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 00:35:35 +0000

    adds sidebarReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "../features/api/apiSlice"
+import sidebarReducer from "../features/sidebar/sidebarSlice"
 
 export const store = configureStore({
   reducer: {
+    sidebar: sidebarReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: getDefaultMiddleware =>
