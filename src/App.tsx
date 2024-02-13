50d3c3bca2db7a636c60a1556f8290a10cf65721
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 00:58:24 +0000

    adds new components to main App component and adds logic to close sidebar when background clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import Home from "./pages/Home"
+import Header from "./common/Header"
+import Footer from "./common/Footer"
+import Nav from "./common/Nav"
+import Sidebar from "./features/sidebar/Sidebar"
+import { useAppDispatch } from "./app/hooks"
+import { closeSidebar } from "./features/sidebar/sidebarSlice"
 
 const App: React.FC = () => {
+
+  const dispatch = useAppDispatch()
+
   return (
-    <main>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-      </Routes>
-    </main>
+    <>
+      <Header />
+      <Nav />
+      <main onClick={() => dispatch(closeSidebar())}>
+        <Sidebar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   )
 }
 
