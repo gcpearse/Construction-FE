53f8b68aae6a8574cf395ef8635dd8e3f66c4e7a
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 14:01:42 +0000

    uses NavLink from react router dom to easily style active menu items

--- src/common/NavMenu.tsx
+++ src/common/NavMenu.tsx
@@ -1,10 +1,21 @@
+import { NavLink } from "react-router-dom"
+
 const NavMenu: React.FC = () => {
+
   return (
-    <ul>
-      <li>Home</li>
+    <ul className="nav-menu">
+      <li>
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </li>
       <li>About</li>
       <li>Jobs</li>
-      <li>Admin</li>
+      <li>
+        <NavLink to="/admin">
+          Admin
+        </NavLink>
+      </li>
     </ul>
   )
 }
