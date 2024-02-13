ba73c50265a9071beac341c48e42ac77137cd6c4
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 01:18:47 +0000

    minor reformatting

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,9 @@ import { job, jobType, personalInfo } from "../../models"
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://4.234.160.181:8080/construction/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://4.234.160.181:8080/construction/api"
+  }),
   endpoints: builder => ({
     getJobs: builder.query<job[], void>({
       query: () => "/jobs"
