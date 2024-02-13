b36f73b862d2635b8e6a7b646d5cafdcb8ec8d46
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 00:55:57 +0000

    renames sidebarSlice reducers for clarity

---
+++ src/features/sidebar/sidebarSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+type SidebarState = {
+  visibleSidebar: boolean
+}
+
+const initialState: SidebarState = {
+  visibleSidebar: false
+}
+
+const sidebarSlice = createSlice({
+  name: "sidebar",
+  initialState,
+  reducers: {
+    toggleSidebar: (state) => {
+      state.visibleSidebar = !state.visibleSidebar
+    },
+    closeSidebar: (state) => {
+      state.visibleSidebar = false
+    }
+  }
+})
+
+export const { toggleSidebar, closeSidebar } = sidebarSlice.actions
+
+export default sidebarSlice.reducer
