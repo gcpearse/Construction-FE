a6c0f2564a114fc90205afbaeaf2442f40d088ab
gcpearse <glen.c.pearse@gmail.com>
2024-02-13 01:19:05 +0000

    creates reusable NavMenu component

---
+++ src/common/NavMenu.tsx
@@ -0,0 +1,12 @@
+const NavMenu: React.FC = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Jobs</li>
+      <li>Admin</li>
+    </ul>
+  )
+}
+
+export default NavMenu
