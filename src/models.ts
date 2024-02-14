43066ada31a71de4869c24585ddaa3f3f84d8d12
gcpearse <glen.c.pearse@gmail.com>
2024-02-14 14:33:03 +0000

    removes commas

--- src/models.ts
+++ src/models.ts
@@ -1,44 +1,44 @@
 export type Job = {
-  job_Id: number,
-  title: string,
-  tagline: string,
-  description: string,
-  job_Type: string,
-  date: string,
-  client: string,
+  job_Id: number
+  title: string
+  tagline: string
+  description: string
+  job_Type: string
+  date: string
+  client: string
   location: string
 }
 
 export type Service = {
-  name: string,
-  description: string,
-  image: string,
+  name: string
+  description: string
+  image: string
   icon: string
 }
 
 export type BusinessInfo = {
-  info_id: number,
-	name: string,
-	email: string,
-	phone: string,
-	address: string,
-	city: string,
-	info: string,
-	logo: string,
-	facebook: string,
-	instagram: string,
-	youtube: string,
-	tiktok: string,
+  info_id: number
+	name: string
+	email: string
+	phone: string
+	address: string
+	city: string
+	info: string
+	logo: string
+	facebook: string
+	instagram: string
+	youtube: string
+	tiktok: string
 	linkedin: string
 }
 
 export type LoginRequest = {
-  name: string,
+  name: string
   password: string
 }
 
 export type LoginResponse = {
-  name: string,
-  token: string,
+  name: string
+  token: string
   role: string
 }
