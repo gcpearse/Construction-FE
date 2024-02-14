ce350dd4bfa45d6e55e39eb33c0644d7c4fca442
gcpearse <glen.c.pearse@gmail.com>
2024-02-14 00:07:23 +0000

    minor reformatting

--- src/models.ts
+++ src/models.ts
@@ -1,33 +1,33 @@
 export type job = {
-  "job_Id": number,
-  "title": string,
-  "tagline": string,
-  "description": string,
-  "job_Type": string,
-  "date": string,
-  "client": string,
-  "location": string
+  job_Id: number,
+  title: string,
+  tagline: string,
+  description: string,
+  job_Type: string,
+  date: string,
+  client: string,
+  location: string
 }
 
 export type service = {
-  "name": string,
-  "description": string,
-  "image": string,
-  "icon": string
+  name: string,
+  description: string,
+  image: string,
+  icon: string
 }
 
 export type businessInfo = {
-  "info_id": number,
-	"name": string,
-	"email": string,
-	"phone": string,
-	"address": string,
-	"city": string,
-	"info": string,
-	"logo": string,
-	"facebook": string,
-	"instagram": string,
-	"youtube": string,
-	"tiktok": string,
-	"linkedin": string
+  info_id: number,
+	name: string,
+	email: string,
+	phone: string,
+	address: string,
+	city: string,
+	info: string,
+	logo: string,
+	facebook: string,
+	instagram: string,
+	youtube: string,
+	tiktok: string,
+	linkedin: string
 }
