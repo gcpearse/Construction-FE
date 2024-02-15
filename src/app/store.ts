ee18b27c609b1e32da27da5d4a29f3f00c31613a
gcpearse <glen.c.pearse@gmail.com>
2024-02-15 18:31:52 +0000

    adds authReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "../features/api/apiSlice"
 import sidebarReducer from "../features/sidebar/sidebarSlice"
+import authReducer from "../features/admin/authSlice"
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+    auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: getDefaultMiddleware =>
