b313a0998bb567b59f924a7580929095ff13510e
gcpearse <glen.c.pearse@gmail.com>
2024-02-15 18:46:38 +0000

    creates basic Dashboard component - currently handles logout only

---
+++ src/features/admin/Dashboard.tsx
@@ -0,0 +1,31 @@
+import { useCookies } from "react-cookie"
+import { useAppDispatch } from "../../app/hooks"
+import { logout } from "./authSlice"
+
+const Dashboard: React.FC = () => {
+
+  const dispatch = useAppDispatch()
+
+  const removeCookie = useCookies(["token"])[2]
+
+  const handleLogout = () => {
+    dispatch(logout())
+    removeCookie("token")
+    localStorage.removeItem("name")
+    localStorage.removeItem("role")
+  }
+
+  return (
+    <div className="logout-wrapper">
+
+      <h2>Dashboard</h2>
+
+      <button onClick={() => handleLogout()}>
+        Log out
+      </button>
+      
+    </div>
+  )
+}
+
+export default Dashboard
