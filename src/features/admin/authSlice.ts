756a7477c722469751c2702211863261f87bbcd3
gcpearse <glen.c.pearse@gmail.com>
2024-02-15 18:32:18 +0000

    builds authSlice

---
+++ src/features/admin/authSlice.ts
@@ -0,0 +1,32 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { LoginResponse, User } from "../../models"
+
+type AuthState = {
+  user: User | null
+  token: string | null
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadAction<LoginResponse>) => {
+      const { name, role, token } = action.payload
+      state.user = { name, role }
+      state.token = token
+    },
+    logout: (state) => {
+      state.user = null
+      state.token = null
+    }
+  }
+})
+
+export const { setCredentials, logout } = authSlice.actions
+
+export default authSlice.reducer
