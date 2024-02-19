ec179d32e51d75b9c42e80d713a14b1148d0ffcc
gcpearse <glen.c.pearse@gmail.com>
2024-02-19 20:15:18 +0000

    uses query to replace hard-coded company name with name in database

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,15 +1,21 @@
 import { FaPhone } from "react-icons/fa"
 import { FaEnvelope } from "react-icons/fa"
+import { useGetBusinessInfoQuery } from "../features/api/apiSlice"
 
 const Header: React.FC = () => {
+
+  const {
+    data: businessInfo
+  } = useGetBusinessInfoQuery()
+
   return (
     <header>
       <ul>
         <li>
-          <FaPhone className="header-icon" />&nbsp; +44 (0) 1234 567 890
+          <FaPhone className="header-icon" />&nbsp; {businessInfo?.phone}
         </li>
         <li>
-          <FaEnvelope className="header-icon" />&nbsp; info@example.example
+          <FaEnvelope className="header-icon" />&nbsp; {businessInfo?.email}
         </li>
       </ul>
     </header>
