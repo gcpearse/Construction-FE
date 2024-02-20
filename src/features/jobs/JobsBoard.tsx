371df4b2e69fcbb334076f0eb8813211c0fcb5c0
gcpearse <glen.c.pearse@gmail.com>
2024-02-20 19:22:27 +0000

    basic setup for displaying jobs on the page

--- src/features/jobs/JobsBoard.tsx
+++ src/features/jobs/JobsBoard.tsx
@@ -1,19 +1,52 @@
 import { Link } from "react-router-dom"
+import { useGetJobsQuery } from "../api/apiSlice"
+import { Job } from "../../models"
+import SingleJob from "./SingleJob"
 
 const JobsBoard: React.FC = () => {
+
+  const {
+    data: jobs,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetJobsQuery()
+
+  let content
+
+  if (isLoading) content = <p className="rtk-query-msg">Loading content...</p>
+
+  if (isError) {
+    console.log(error)
+    content = <p className="rtk-query-msg">Oops! Something went wrong...</p>
+  }
+
+  if (isSuccess) content = (
+    <ul>
+      {jobs?.map((job: Job) => {
+        return (
+          <li key={job.job_Id}><SingleJob job={job} /></li>
+        )
+      })}
+    </ul>
+  )
+
   return (
     <div className="page-wrapper">
 
-    <div className="page-top">
-      <h2>Jobs</h2>
-      <Link to="/admin">
-        <button className="dash-nav-btn">
-          Dashboard
-        </button>
-      </Link>
-    </div>
+      <div className="page-top">
+        <h2>Jobs</h2>
+        <Link to="/admin">
+          <button className="dash-nav-btn">
+            Dashboard
+          </button>
+        </Link>
+      </div>
 
-  </div>
+      {content}
+
+    </div>
   )
 }
 
