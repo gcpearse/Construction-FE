a4c99044e1fdf757e8fe61daac5c9ea17462e143
gcpearse <glen.c.pearse@gmail.com>
2024-02-22 01:48:06 +0000

    adds icon to logout button and moves file to new folder

---
+++ src/features/admin/dashboard/userProfile/Logout.tsx
@@ -0,0 +1,28 @@
+import { useCookies } from "react-cookie"
+import { useAppDispatch } from "../../../../app/hooks"
+import { logout } from "../../authSlice"
+import { MdLogout } from "react-icons/md"
+
+const Logout: React.FC = () => {
+
+  const dispatch = useAppDispatch()
+
+  const removeCookie = useCookies(["token"])[2]
+
+  const handleLogout = () => {
+    dispatch(logout())
+    removeCookie("token")
+    localStorage.removeItem("name")
+    localStorage.removeItem("role")
+  }
+
+  return (
+    <button
+      className="logout-btn"
+      onClick={() => handleLogout()}>
+      <MdLogout />&nbsp;Log out
+    </button>
+  )
+}
+
+export default Logout
