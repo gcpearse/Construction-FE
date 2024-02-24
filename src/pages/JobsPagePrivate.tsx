136b969bf5fa52e6c3bd3bfb32ac981058524f19
gcpearse <glen.c.pearse@gmail.com>
2024-02-24 19:39:02 +0000

    strengthens auth system by incorporating useTestAuthQuery following back-end fix

--- src/pages/JobsPagePrivate.tsx
+++ src/pages/JobsPagePrivate.tsx
@@ -1,14 +1,19 @@
 import { useCookies } from "react-cookie"
 import Login from "../features/admin/Login"
 import JobsBoard from "../features/jobs/JobsBoard"
+import { useTestAuthQuery } from "../features/api/apiSlice"
 
 const JobsPagePrivate: React.FC = () => {
 
-  const [cookies] = useCookies(["token"])
+  const [{ token }] = useCookies(["token"])
+
+  const {
+    isSuccess
+  } = useTestAuthQuery(token)
 
   return (
     <section>
-      {!cookies.token ? (
+      {!isSuccess ? (
         <Login />
       ) : (
         <JobsBoard />
