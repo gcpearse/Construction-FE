113679aff62190e13100cc58a7c1694b72f581e4
gcpearse <glen.c.pearse@gmail.com>
2024-02-27 01:33:22 +0000

    builds structure of ServiceAdder form - query logic to follow

---
+++ src/features/services/ServiceAdder.tsx
@@ -0,0 +1,112 @@
+import { FaRegWindowClose } from "react-icons/fa"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { closeServiceAdder } from "./servicesSlice"
+import { Service } from "../../models"
+import { useForm } from "react-hook-form"
+import { useState } from "react"
+
+const ServiceAdder: React.FC = () => {
+
+  const dispatch = useAppDispatch()
+
+  const { isServiceAdderToggled } = useAppSelector(state => state.services)
+
+  const [charLimit, setCharLimit] = useState<number>(0)
+
+  const {
+    register
+  } = useForm<Service>()
+
+  return (
+    <div className={isServiceAdderToggled ? (
+      "modal-form-overlay"
+    ) : (
+      "modal-form-overlay closed-modal"
+    )}>
+
+      <div className="modal-form-wrapper">
+
+        <div className="modal-form-top">
+          <h3>Create Service</h3>
+          <button
+            className="window-close-btn"
+            onClick={() => {
+              dispatch(closeServiceAdder())
+              document.body.style.overflow = "auto"
+            }}>
+            <FaRegWindowClose className="window-close-icon" />
+          </button>
+        </div>
+
+        <form>
+
+          <label htmlFor="service-name-input">
+            Service name: <span>*</span>
+          </label>
+          <input
+            type="text"
+            id="service-name-input"
+            autoComplete="true"
+            required
+            {...register("name")} />
+
+          <label htmlFor="service-icon-input">
+            Service icon: <span>*</span>
+          </label>
+          <input
+            type="text"
+            id="service-icon-input"
+            autoComplete="true"
+            required
+            {...register("icon")} />
+
+          <label htmlFor="service-description-input">
+            Description of service: <span>*</span>
+          </label>
+          <textarea
+            rows={5}
+            id="service-description-input"
+            autoComplete="true"
+            maxLength={200}
+            required
+            {...register("description", {
+              onChange: (e) => {
+                setCharLimit(e.target.value.length)
+              }
+            })} />
+
+          <p>{200 - charLimit} characters remaining.</p>
+
+          <label htmlFor="service-image-input">
+            Select an image: <span>*</span>
+          </label>
+          <input
+            type="file"
+            style={{
+              marginTop: "0.5em",
+              border: "none",
+              borderRadius: 0,
+              boxShadow: "none",
+              padding: 0
+            }}
+            id="service-image-input"
+            required
+            {...register("image")} />
+
+          <p>* Indicates a required field.</p>
+
+          <button
+            type="submit"
+            className="modal-btn">
+            Submit
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default ServiceAdder
