49233d2424700339f6e9a02ceebe6dc5506ceb29
gcpearse <glen.c.pearse@gmail.com>
2024-02-27 00:59:46 +0000

    creates separate component for dash nav button

---
+++ src/common/DashNavBtn.tsx
@@ -0,0 +1,22 @@
+import { MdDashboard } from "react-icons/md"
+import { Link } from "react-router-dom"
+
+const DashNavBtn: React.FC = () => {
+  return (
+    <>
+      <Link to="/admin">
+        <button className="blue-btn large-display-el">
+          <p>Dashboard</p>
+        </button>
+      </Link>
+
+      <Link to="/admin">
+        <button className="blue-icon-btn icon-btn small-display-el">
+          <MdDashboard className="btn-icon" />
+        </button>
+      </Link>
+    </>
+  )
+}
+
+export default DashNavBtn
