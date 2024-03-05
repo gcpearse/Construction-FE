3fcdbc81d90151d547972b65e0c492cb7f8648b0
gcpearse <glen.c.pearse@gmail.com>
2024-03-05 14:13:10 +0000

    creates new util to check jobs by service name in params

--- src/utils/logicalUtils.ts
+++ src/utils/logicalUtils.ts
@@ -1,8 +1,17 @@
 import { Job, Service } from "../models"
 
+
 export const checkJobs = (jobs: Job[], service: Service): boolean => {
 
   return Boolean(jobs.filter((job) => {
     return job.job_Type === service.name
+  }).length)
+}
+
+
+export const checkJobsByParams = (jobs: Job[], service: string | undefined): boolean => {
+
+  return Boolean(jobs.filter((job) => {
+    return job.job_Type === service
   }).length)
 }
