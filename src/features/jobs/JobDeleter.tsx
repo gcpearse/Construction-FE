4ff59d4521641202b2b3e3c3d14abb473165401e
gcpearse <glen.c.pearse@gmail.com>
2024-03-07 22:00:34 +0000

    adds form handling and completes functionality for deleting jobs by ID

--- src/features/jobs/JobDeleter.tsx
+++ src/features/jobs/JobDeleter.tsx
@@ -3,10 +3,19 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { Job } from "../../models"
 import { closeJobDeleter } from "./jobsSlice"
 import { formatHeader } from "../../utils/formattingUtils"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { useCookies } from "react-cookie"
+import { useState } from "react"
+import { useDeleteJobMutation } from "../api/apiSlice"
+import { useNavigate } from "react-router-dom"
 
 
 type Props = {
   job: Job
+}
+
+type FormValues = {
+  name: string
 }
 
 
@@ -16,6 +25,59 @@ const JobDeleter: React.FC<Props> = ({ job }) => {
   const dispatch = useAppDispatch()
 
   const { isJobDeleterToggled } = useAppSelector(state => state.jobs)
+
+  const navigate = useNavigate()
+
+  const [{ token }] = useCookies(["token"])
+
+  const [errorMsg, setErrorMsg] = useState<string>("")
+
+  const [deleteJob] = useDeleteJobMutation()
+
+  const {
+    register,
+    handleSubmit,
+    reset
+  } = useForm<FormValues>()
+
+
+  const submitForm: SubmitHandler<FormValues> = async ({ name }) => {
+
+    const currentUser = localStorage.getItem("name")
+
+    if (currentUser === name) {
+      try {
+        await deleteJob({
+          id: job.job_Id,
+          token: token
+        }).unwrap()
+
+        reset()
+
+        dispatch(closeJobDeleter())
+
+        document.body.style.overflow = "auto"
+
+        setErrorMsg("")
+
+        navigate(`/admin/jobs/${job.job_Type}`)
+
+      } catch (error: any) {
+
+        console.log(error)
+
+        if (error.status === 401) {
+          setErrorMsg("Authentication error. Please refresh the page.")
+        } else {
+          setErrorMsg("Oops! Something went wrong...")
+        }
+
+      }
+
+    } else {
+      setErrorMsg("Incorrect admin name.")
+    }
+  }
 
 
   return (
@@ -50,7 +112,29 @@ const JobDeleter: React.FC<Props> = ({ job }) => {
 
         </div>
 
-        <form>
+        <form
+          onSubmit={handleSubmit(submitForm)}
+          onChange={() => setErrorMsg("")}>
+
+          <label htmlFor={`delete-job-name-input`}>
+            Admin name:
+          </label>
+
+          <input
+            type="text"
+            id={`delete-job-name-input`}
+            autoComplete="true"
+            required
+            {...register("name")} />
+
+          <button
+            type="submit"
+            className="modal-btn blue-btn">
+            DELETE
+          </button>
+
+          {errorMsg ? <p className="rtk-query-form-msg">{errorMsg}</p> : null}
+
         </form>
       </div>
     </div>
