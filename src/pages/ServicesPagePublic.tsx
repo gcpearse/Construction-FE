2af63e4b8886cca9128aec040a469e6e9477c71a
gcpearse <glen.c.pearse@gmail.com>
2024-03-14 00:12:37 +0000

    adds content to public services page

--- src/pages/ServicesPagePublic.tsx
+++ src/pages/ServicesPagePublic.tsx
@@ -1,19 +1,70 @@
-import { Link } from "react-router-dom"
+import { useGetBusinessInfoQuery, useGetServicesQuery } from "../features/api/apiSlice"
+import PublicService from "../features/services/services-public/PublicService"
+import { Service } from "../models"
 
 
 const ServicesPagePublic: React.FC = () => {
 
 
+  const {
+    data: businessInfo
+  } = useGetBusinessInfoQuery()
+
+  const {
+    data: services,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetServicesQuery()
+
+
+  let content
+
+  if (isLoading) content = <p className="rtk-query-msg">Loading content...</p>
+
+  if (isError) {
+    console.log(error)
+    content = <p className="rtk-query-msg">Oops! Something went wrong...</p>
+  }
+
+  if (isSuccess) content = (
+    <ul>
+
+      {[...services].sort((a, b) => {
+        if (a.name > b.name) return 1
+        if (a.name < b.name) return -1
+        return 0
+      }).map((service: Service) => {
+        return (
+          <li
+            key={service.name}
+            className={services.indexOf(service) % 2 ? (
+              "public-service-wrapper left-card"
+            ) : (
+              "public-service-wrapper right-card"
+            )}>
+            <PublicService service={service} />
+          </li>
+        )
+      })}
+
+      <div className="public-service-final-el">
+        <span>{businessInfo?.name}</span>
+      </div>
+
+    </ul>
+  )
+
+
   return (
-    <div className="temp-wrapper">
+    <section className="service-page-public-wrapper">
 
-      <h2>Hello</h2>
+      <h2>Our Services</h2>
 
-      <p>This page is currently under construction.</p>
+      {content}
 
-      <p>If you have user credentials, you can log in and view the admin dashboard <Link to="/admin"><span>here</span></Link>.</p>
-
-    </div>
+    </section>
   )
 }
 
