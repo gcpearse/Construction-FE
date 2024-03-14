89ef883baed092467473fbe00f63bf1d74d8b55d
gcpearse <glen.c.pearse@gmail.com>
2024-03-14 20:49:02 +0000

    adds content to Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,85 @@
 import { Link } from "react-router-dom"
+import { useGetBusinessInfoQuery } from "../features/api/apiSlice"
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa"
 
 
 const HomePage: React.FC = () => {
 
 
-  return (
-    <div className="temp-wrapper">
+  const {
+    data: businessInfo,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetBusinessInfoQuery()
 
-      <h2>Hello</h2>
 
-      <p>This page is currently under construction.</p>
+  let content
 
-      <p>If you have user credentials, you can log in and view the admin dashboard <Link to="/admin"><span>here</span></Link>.</p>
+  if (isLoading) content = <p className="rtk-query-msg">Loading content...</p>
 
+  if (isError) {
+    console.log(error)
+    content = <p className="rtk-query-msg">Oops! Something went wrong...</p>
+  }
+
+  if (isSuccess) content = (
+    <div className="home-page-content">
+
+      <h2>{businessInfo.name}</h2>
+
+      <p>{businessInfo.info}</p>
+
+      <Link
+        className="home-page-link"
+        to="/services">
+        View our services
+      </Link>
+
+      <div className="home-page-icons-wrapper">
+
+        {businessInfo.facebook ? (
+          <FaFacebook />
+        ) : (
+          null
+        )}
+
+        {businessInfo.instagram ? (
+          <FaInstagram />
+        ) : (
+          null
+        )}
+
+        {businessInfo.youtube ? (
+          <FaYoutube />
+        ) : (
+          null
+        )}
+
+        {businessInfo.linkedin ? (
+          <FaLinkedin />
+        ) : (
+          null
+        )}
+
+        {businessInfo.tiktok ? (
+          <FaTiktok />
+        ) : (
+          null
+        )}
+
+      </div>
     </div>
+  )
+
+
+  return (
+    <section className="home-page-wrapper">
+
+      {content}
+
+    </section>
   )
 }
 
